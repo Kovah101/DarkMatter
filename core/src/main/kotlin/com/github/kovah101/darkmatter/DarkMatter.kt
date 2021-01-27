7c540c2c0f401769cdd1aaace1efda3e0572e4e2
Elliot Thompson <elliotthompson101@gmail.com>
2021-01-27 15:48:31 +0000

    Added Ktx-app & Ktx-log extensions in gradle, created abstract DarkMatterScreen class to allow screens to change active screen

--- core/src/main/kotlin/com/github/kovah101/darkmatter/DarkMatter.kt
+++ core/src/main/kotlin/com/github/kovah101/darkmatter/DarkMatter.kt
@@ -1,11 +1,24 @@
 package com.github.kovah101.darkmatter
 
-import com.badlogic.gdx.Game
-import com.github.kovah101.darkmatter.FirstScreen
+import com.badlogic.gdx.Application.LOG_DEBUG
+import com.badlogic.gdx.Gdx
+import com.github.kovah101.darkmatter.screen.FirstScreen
+import com.github.kovah101.darkmatter.screen.SecondScreen
+import ktx.app.KtxGame
+import ktx.app.KtxScreen
+import ktx.log.debug
+import ktx.log.logger
 
-/** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms.  */
-class DarkMatter : Game() {
+private val LOG = logger<DarkMatter>()
+
+class DarkMatter : KtxGame<KtxScreen>() {
     override fun create() {
-        setScreen(FirstScreen())
+        Gdx.app.logLevel = LOG_DEBUG
+        LOG.debug {"Create game instance"}
+        // must add screens
+        addScreen(FirstScreen(this))
+        addScreen(SecondScreen(this))
+
+        setScreen<FirstScreen>()
     }
 }
---
+++ core/src/main/kotlin/com/github/kovah101/darkmatter/screen/DarkMatterScreen.kt
@@ -0,0 +1,6 @@
+package com.github.kovah101.darkmatter.screen
+
+import com.github.kovah101.darkmatter.DarkMatter
+import ktx.app.KtxScreen
+
+abstract class DarkMatterScreen (val game: DarkMatter) : KtxScreen
