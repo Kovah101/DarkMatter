f7425c9e958938a1ba1c7234643b64975f21ec0e
Elliot Thompson <elliotthompson101@gmail.com>
2021-02-24 10:18:06 +0000

    Created enemy on projectile hit detection

--- core/src/main/kotlin/com/github/kovah101/darkmatter/ecs/system/EnemySystem.kt
+++ core/src/main/kotlin/com/github/kovah101/darkmatter/ecs/system/EnemySystem.kt
@@ -2,12 +2,14 @@ package com.github.kovah101.darkmatter.ecs.system
 
 import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.systems.IteratingSystem
+import com.badlogic.gdx.math.Rectangle
 import com.github.kovah101.darkmatter.ecs.components.*
 import com.github.kovah101.darkmatter.event.GameEventManager
 import ktx.ashley.addComponent
 import ktx.ashley.allOf
 import ktx.ashley.exclude
 import ktx.ashley.get
+import ktx.log.debug
 import ktx.log.logger
 
 private val LOG = logger<EnemySystem>()
@@ -17,8 +19,15 @@ class EnemySystem(
 ) : IteratingSystem(
     allOf(EnemyComponent::class, TransformComponent::class).exclude(RemoveComponent::class).get()
 ) {
+    private val projectileBoundRect = Rectangle()
+    private val enemyBoundRect = Rectangle()
 
-
+    // by lazy to initialise later
+    private val projectileEntities by lazy {
+        engine.getEntitiesFor(
+            allOf(ProjectileComponent::class).exclude(RemoveComponent::class).get()
+        )
+    }
 
 
     override fun processEntity(entity: Entity, deltaTime: Float) {
@@ -35,5 +44,36 @@ class EnemySystem(
             entity.addComponent<RemoveComponent>(engine)
             return
         }
+
+        //create bounds for enemies and projectiles
+        enemyBoundRect.set(
+            transform.position.x,
+            transform.position.y,
+            transform.size.x,
+            transform.size.y
+        )
+
+        projectileEntities.forEach { projectile ->
+            projectile[TransformComponent.mapper]?.let { projTrans ->
+                projectileBoundRect.set(
+                    projTrans.position.x,
+                    projTrans.position.y,
+                    projTrans.size.x,
+                    projTrans.size.y
+                )
+
+                if (projectileBoundRect.overlaps(enemyBoundRect)) {
+                    damageEnemy(entity, projectile)
+                }
+
+            }
+
+        }
+
+    }
+
+    private fun damageEnemy(enemy : Entity, projectile : Entity){
+        LOG.debug { "Enemy hit!" }
+        // TODO damage and destroy both projectile and enemy
     }
 }
